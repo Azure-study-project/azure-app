75988216b886f3dab2e97efc5e4932fe6f1c5410
yus04 <yuswim192@gmail.com>
2023-05-11 23:32:00 +0900

    move client.js to recyled folder

--- javascript/recycled/client.js
+++ javascript/recycled/client.js
@@ -5,26 +5,28 @@ class MSGraphClient {
     this.tenantId = process.env.TENANT_ID
     this.clientId = process.env.CLIENT_ID
     this.clientSecret = process.env.CLIENT_SECRET
+    this.scopes = process.env.SCOPES.split(',');
+    this.msgraphClient = "@microsoft/microsoft-graph-client"
   }
 
   // 認証プロバイダーを定義し、クライアントを初期化するために必要なオプションを設定
-  setOptions(credential){
-    const options = {
+  getDefaultProviders(credential){
+    const providers = {
       authProvider: async (done) => {
         const accessToken = await credential.getToken("https://graph.microsoft.com/.default");
         done(null, accessToken.token);
       },
     };
-    return options;
+    return providers;
   }
 
   // ClientSecretCredentialを使用して認証情報を取得し、MS Graph APIクライアントを初期化
-  getClient(){
+  getSecretClient(){
     const { ClientSecretCredential } = require("@azure/identity");
     const credential = new ClientSecretCredential(this.tenantId, this.clientId, this.clientSecret);
-    const options = this.setOptions(credential);
-    var { Client } = require("@microsoft/microsoft-graph-client");
-    const client = Client.init(options);
+    const providers = this.getDefaultProviders(credential);
+    var { Client } = require(this.msgraphClient);
+    const client = Client.init(providers);
     return client;
   }
 }
@@ -32,6 +34,6 @@ class MSGraphClient {
 // MS Graph APIクライアントをエクスポートする関数を定義
 module.exports = function(){
   const msgc = new MSGraphClient();
-  const client = msgc.getClient();
+  const client = msgc.getSecretClient();
   return client;
 }
